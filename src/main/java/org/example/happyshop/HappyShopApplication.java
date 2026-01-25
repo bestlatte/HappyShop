f2bc2ae2403a4af42f9c1d5c0265883888a099e4
bestlatte <x123456789xxxxxxtw@gmail.com>
2026-01-25 18:14:21 +0800

    前端react環境建置 分成兩個模組 一般使用者的web 以及 後台管理admin

---
+++ src/main/java/org/example/happyshop/HappyShopApplication.java
@@ -0,0 +1,13 @@
+package org.example.happyshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HappyShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/happyshop/HappyShopApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.happyshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappyShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
