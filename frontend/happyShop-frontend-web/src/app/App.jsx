1c82fce8d383605db3ce0df35345a4eea60597d1
bestlatte <x123456789xxxxxxtw@gmail.com>
2026-02-12 20:24:50 +0800

    feat(APP):更改成route路徑

--- frontend/happyShop-frontend-web/src/app/App.jsx
+++ frontend/happyShop-frontend-web/src/app/App.jsx
@@ -1,12 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import RootLayout from "../layouts/RootLayout";
+import Home from "../features/product/pages/Home.jsx";
+import ProductBrowser from "../features/productBrowser/pages/ProductBrowser.jsx";
 
 export default function App() {
+    const user = null; // 如果要給 Home，也可在此處建立 user
+
     return (
-        <RootLayout >
-            <h1 className="text-2xl font-bold">首頁</h1>
-            <p className="mt-2 text-gray-600">
-                放商品列表
-            </p>
-        </RootLayout>
+        <BrowserRouter>
+            <Routes>
+                <Route element={<RootLayout />}>
+                    {/* 傳入 user 給 Home（若 Home 需要） */}
+                    {/*<Route path="/" element={<Home user={user} />} />*/}
+                    <Route path="/" element={<ProductBrowser  />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+
     );
 }
+
