f4f32d19ec2c9a77156291c0c51f449217e3d206
bestlatte <x123456789xxxxxxtw@gmail.com>
2026-03-15 20:56:14 +0800

    refactor : 1.更改home以及ProductBrowser mockProducts 檔案名稱 2.

---
+++ frontend/happyShop-frontend-web/src/features/product/pages/HomePage.jsx
@@ -0,0 +1,15 @@
+import ProductSection from "../sections/ProductSection.jsx";
+
+export default function HomePage() {
+
+
+
+    return (
+        <div className="py-10 border-bS border-b border-black/10">
+
+            <ProductSection title="新品上市" tag="New" nav="topic"    categoryKey="new" />
+            <ProductSection title="Tense 韓國套組" tag="Tense"  nav="topic" categoryKey="tense" />
+
+        </div>
+    );
+}
