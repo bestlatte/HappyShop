d05eb4d271b6783bbdd9325325f8fac0a90b24c1
akana627 <kuzmi00627@gmail.com>
2026-03-22 20:55:33 +0800

    refactor: 修正相關樣式Warnings

--- frontend/happyShop-frontend-web/src/features/products/components/product-info/InfoShipping.jsx
+++ frontend/happyShop-frontend-web/src/features/products/components/product-info/InfoShipping.jsx
@@ -23,7 +23,7 @@ export const InfoShipping = ({ shippingMethods }) => {
       {/* --- Content --- */}
       <div
         className={`overflow-hidden transition-all duration-500 ease-in-out ${
-          isOpen ? "max-h-[500px] opacity-100" : "max-h-0 opacity-0"
+          isOpen ? "max-h-125 opacity-100" : "max-h-0 opacity-0"
         }`}
       >
         <div className="pb-6">
