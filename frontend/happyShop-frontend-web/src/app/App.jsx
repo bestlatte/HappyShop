5604d0f6cdc833f8ce709d543442f0611b57c93a
akana627 <kuzmi00627@gmail.com>
2026-03-29 01:26:33 +0800

    feat :

--- frontend/happyShop-frontend-web/src/app/App.jsx
+++ frontend/happyShop-frontend-web/src/app/App.jsx
@@ -5,27 +5,30 @@
 // }
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { CartProvider } from "./contexts/CartContext";
 import RootLayout from "../layouts/RootLayout";
-import ProductDetailPage from "../features/products/pages/ProductDetailPage";
+import ProductDetailPage from "../features/productsDetail/pages/ProductDetailPage";
 import { ProductLayout } from "../layouts/ProductLayout";
 import { CartLayout } from "../layouts/CartLayout";
 import CartPage from "../features/cart/pages/CartPage";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        //home page
-        <Route path="/" element={<RootLayout children="你好" />} />
-        //product detail page
-        <Route element={<ProductLayout />}>
-          <Route path="/products/:id" element={<ProductDetailPage />} />
-        </Route>
-        //cart page
-        <Route element={<CartLayout />}>
-          <Route path="/cart" element={<CartPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <CartProvider>
+      <BrowserRouter>
+        <Routes>
+          //home page
+          <Route path="/" element={<RootLayout children="你好" />} />
+          //product detail page
+          <Route element={<ProductLayout />}>
+            <Route path="/products/:id" element={<ProductDetailPage />} />
+          </Route>
+          //cart page
+          <Route element={<CartLayout />}>
+            <Route path="/cart" element={<CartPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </CartProvider>
   );
 }
